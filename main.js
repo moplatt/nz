ed80843f06fcca842add2b550f43368cc6a090ce
moplatt <moritz.plattner03@gmail.com>
2025-03-20 18:17:43 +0100

    js file hinzugefügt & shortcut icon eingebaut

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hallo");
+var map = L.map('map').setView([-46.98, 167.88], 9);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([-46.98, 167.88]).addTo(map);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
