b600c3c1eb04bedc998978b4c35bb8fa4417edda
moplatt <moritz.plattner03@gmail.com>
2025-03-27 15:49:46 +0100

    variablen lat, lon, zoom erstellt

--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 Script für die Neuseelandreise
 */
 
+let lat = -46.98;
+let lon = 167.88;
+let zoom = 9
+
+
 // Karte initialisieren
-let map = L.map('map').setView([-46.98, 167.88], 9);
+let map = L.map('map').setView([lat, lon], zoom);
 //Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         // Marker zeichnen
-        let marker = L.marker([-46.98, 167.88]).addTo(map);
+        let marker = L.marker([lat, lon]).addTo(map);
         // Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
