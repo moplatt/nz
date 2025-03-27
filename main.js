44e86b30fe26f939bcba70f24446648c3eefd9b0
moplatt <moritz.plattner03@gmail.com>
2025-03-27 16:31:40 +0100

    stop obj in karte verwendet

--- main.js
+++ main.js
@@ -2,35 +2,31 @@
 Script für die Neuseelandreise
 */
 
-let lat = -46.98;
-let lon = 167.88;
-let zoom = 9;
-
 let stop = {
     nr: 21,
-    titel: "Steward Island",
+    title: "Steward Island",
     user: "moplatt",
-    lat: lat,
-    lng: lon,
-    zoom: zoom,
+    lat: -46.98,
+    lng: 167.88,
+    zoom: 9,
 };
 
 // Karte initialisieren
-let map = L.map('map').setView([lat, lon], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 //Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         // Marker zeichnen
-        let marker = L.marker([lat, lon]).addTo(map);
+        let marker = L.marker([stop.lat, stop.lng]).addTo(map);
         // Popup definieren und öffnen
         marker.bindPopup(`
-            <h2><b>Steward Island!</b></h2>
+            <h2><b>${stop.title}</b></h2>
             <br> 
             <ul>
-                <li>geogr. Länge: ${lat.toFixed(5)}°</li>
-                <li>geogr. Breite ${lon.toFixed(5)}°</li>
+                <li>geogr. Länge: ${stop.lat.toFixed(5)}°</li>
+                <li>geogr. Breite ${stop.lng.toFixed(5)}°</li>
             </ul>
         `).openPopup();
 
