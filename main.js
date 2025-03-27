ed155f4e70a3864279e56fd6e9cc2c0e59bd5ca7
moplatt <moritz.plattner03@gmail.com>
2025-03-27 15:39:00 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ Script für die Neuseelandreise
 */
 
 // Karte initialisieren
-var map = L.map('map').setView([-46.98, 167.88], 9);
+let map = L.map('map').setView([-46.98, 167.88], 9);
 //Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         // Marker zeichnen
-        var marker = L.marker([-46.98, 167.88]).addTo(map);
+        let marker = L.marker([-46.98, 167.88]).addTo(map);
         // Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
